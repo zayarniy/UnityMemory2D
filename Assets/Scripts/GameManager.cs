92e743f6828f346b4019a7a179da1361a4deb046
Ваше имя <zaazaa@yandex.ru>
2023-04-18 23:17:22 +0300

    Игра готова. Можно использовать как заготовку

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using DG.Tweening;
 
 
@@ -11,46 +12,82 @@ public class GameManager : MonoBehaviour
 
     public Transform ThisTransform;   
     public Sprite[] sprites;
-    public Image[] images;
+    //public Image[] images;
     public Canvas canvas;
+    public GameObject panel;
     public Sprite  Cover;
-
+    public TMPro.TextMeshProUGUI textCounter, textTimer;
+    Canvas copyCanvas;
+    int counter;
 
     Transform lastTrans,currentTrans;
 
     int clickCount = 0;
-    Canvas copyCanvas;
+    
     
     // Start is called before the first frame update
     void Start()
     {
         clickCount = 0;
+        counter = 0;
+        startGame = System.DateTime.Now;
         DOTween.defaultAutoKill = false;
         lastTrans = null;
-        
-        //ThisTransform = GetComponent<Transform>();
-        for(int i=0;i<2*sprites.Length;i++)
-        {
-            GameObject go = new GameObject(sprites[i % sprites.Length].name);
-            
-            go.transform.SetParent(canvas.transform);
-            go.AddComponent<UnityEngine.UI.Image>();
-            go.GetComponent<UnityEngine.UI.Image>().sprite = sprites[i% sprites.Length];
-            go.AddComponent<Button>();
 
+        //ThisTransform = GetComponent<Transform>();
+        Shuffle(20);
+        List<Sprite> list = new List<Sprite>();
+        for (int i = 0; i < sprites.Length; i++)
+            list.Add(sprites[i]);
+
+        for (int i=0;i<sprites.Length;i++)
+        {
+            GameObject go = new GameObject(sprites[i].name);            
+            go.transform.SetParent(panel.transform);
+            go.AddComponent<UnityEngine.UI.Image>();
+            go.GetComponent<UnityEngine.UI.Image>().sprite = sprites[i];
+            go.AddComponent<Button>();
         }
+
+        while (list.Count != 0)
+        {
+            int index = Random.Range(0, list.Count);
+            GameObject go = new GameObject(list[index].name);
+            go.transform.SetParent(panel.transform);
+            go.AddComponent<UnityEngine.UI.Image>();
+            go.GetComponent<UnityEngine.UI.Image>().sprite = list[index];
+            go.AddComponent<Button>();
+            list.RemoveAt(index);
+        }
+
         copyCanvas = Instantiate(canvas);
-        foreach (Transform transform in copyCanvas.transform)
+        var pan = copyCanvas.transform.GetChild(0);
+        foreach (Transform transform in pan.transform)
 
         {
             print(transform);
-            transform.GetComponent<UnityEngine.UI.Image>().sprite = Cover;
-            Button button=transform.GetComponent<Button>();
-            button.onClick.AddListener(delegate { Flip(transform); });
-            
+            if (transform.name != "Panel")
+            {
+                transform.GetComponent<UnityEngine.UI.Image>().sprite = Cover;
+                Button button = transform.GetComponent<Button>();
+                button.onClick.AddListener(delegate { Flip(transform); });
+            }
             
         }
 
+    }
+
+    void Shuffle(int n)
+    {
+
+        for(int i=0;i<n;i++)
+        {
+            int index1 = Random.Range(0, sprites.Length);
+            int index2 = Random.Range(0, sprites.Length);
+            Object temp = sprites[index1];
+            sprites[index1] = sprites[index2];
+            sprites[index2] = temp as Sprite;
+        }
     }
 
     void RewindAnim()
@@ -68,7 +105,8 @@ public class GameManager : MonoBehaviour
     {
         if (clickCount<2)
         {
-            clickCount++;
+            clickCount++;            
+            //textCounter.text = (++counter).ToString();
             print(lastTrans);
             //transform.DOFlip();
             //ThisTransform.DOMove(new Vector3(10, 0, 0), 1);
@@ -107,8 +145,19 @@ public class GameManager : MonoBehaviour
     }
 
     // Update is called once per frame
+
+    float spentTime = 0;
+    System.DateTime startGame=System.DateTime.Now;
+
     void Update()
     {
-        
+        spentTime += Time.deltaTime;
+        if (spentTime>=1)
+        {
+          //  textTimer.text = (System.DateTime.Now - startGame).TotalSeconds.ToString();
+            spentTime = 0;
+        }
     }
+
+
 }
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+    public UnityEngine.Audio.AudioMixer audioMixer;
+    public UnityEngine.UI.Slider sliderVolume;
+    
+    public void LoadScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    //https://youtu.be/C1gCOoDU29M
+    public void ValueChange()
+    {
+        audioMixer.SetFloat("MasterVolume", Mathf.Log10(sliderVolume.value)*20);
+    }
+}
